5da41ea29bf9687b4917e4d0baf91f13393df034
EvilDeadLoc <rishabnawani8@gmail.com>
2025-10-07 00:37:53 +0530

    Time: 10 ms (41.08%) | Memory: 15.2 MB (6.60%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -10,6 +10,35 @@ class Solution {
 public:
     bool hasCycle(ListNode *head) {
 
+        // Initialize a pointer 'temp'
+        // at the head of the linked list
+        ListNode* temp = head;  
+        
+        // Create a map to keep track of
+        // encountered nodes
+        std::unordered_map<ListNode*, int> nodeMap;  
+
+        // Step 2: Traverse the linked list
+        while (temp != nullptr) {
+            // If the node is already in the
+            // map, there is a loop
+            if (nodeMap.find(temp) != nodeMap.end()) {
+                return true;
+            }
+            // Store the current node
+            // in the map
+            nodeMap[temp] = 1;
+            
+            // Move to the next node
+            temp = temp->next;  
+        }
+
+        // Step 3: If the list is successfully traversed 
+        // without a loop, return false
+        return false;
+
+/*
+
         ListNode* slow=head;
         ListNode* fast=head;
 
@@ -22,6 +51,9 @@ public:
         }
 
         return false;
+
+
+*/
 
         /*this fails cause if the it has a loop, while condition wont terminate at all as it doesnt reach null, hence will never break hence infinity
 
