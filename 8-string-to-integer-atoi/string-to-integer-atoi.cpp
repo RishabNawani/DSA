7b4645c0fba1a3c7322f3491f991b18ccfc2f6a3
EvilDeadLoc <rishabnawani8@gmail.com>
2025-10-17 00:01:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (54.75%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,5 +1,17 @@
 class Solution {
 public:
+    int nummm(const string& s,int i,long long num,int sign){
+        if(i>s.size() || !isdigit(s[i])){
+            return (int)(sign*num);
+        }
+
+        num=num*10+(s[i]-'0');
+
+        if(sign*num>=INT_MAX) return INT_MAX;
+        if(sign*num<=INT_MIN) return INT_MIN;
+
+        return nummm(s,i+1,num,sign);
+    }
     int myAtoi(string s) {
         int i=0, n=s.size();
         long result=0;
@@ -15,6 +27,9 @@ public:
             }
             i++;
         }
+        return nummm(s,i,0,sign);
+
+        /*
 
         while(i<n && isdigit(s[i])){
             result=result*10+(s[i]-'0');
@@ -25,6 +40,6 @@ public:
         }
 
         return (int)(sign*result);
-
+*/
     }
 };
