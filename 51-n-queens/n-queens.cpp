ae77534ce766d230eee116a6ae326691fc6b0780
EvilDeadLoc <rishabnawani8@gmail.com>
2025-10-24 17:19:53 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (74.39%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-
+/*
     bool issafe(int row, vector<string>& board, int col, int n){
         int duprow= row;
         int dupcol=col;
@@ -44,6 +44,29 @@ public:
         }
     }
 
+*/
+
+    void solve(int col, int n, vector<string>& res, vector<vector<string>>& result, vector<int>& leftrow, vector<int>& upperdiag, vector<int>& lowerdiag){
+        if(col==n){
+            result.push_back(res);
+            return;
+        }
+
+        for(int i=0; i<n;i++){
+            if(leftrow[i]==0 && lowerdiag[i+col]==0 && upperdiag[n-1+col-i]==0){
+                res[i][col]='Q';
+                leftrow[i]=1;
+                lowerdiag[i+col]=1;
+                upperdiag[n-1+col-i]=1;
+                solve(col+1,n,res,result,leftrow,upperdiag,lowerdiag);
+                res[i][col]='.';
+                leftrow[i]=0;
+                lowerdiag[i+col]=0;
+                upperdiag[n-1+col-i]=0;
+            }
+        }
+    }
+
 
     vector<vector<string>> solveNQueens(int n) {
         vector<string> res(n);
@@ -52,7 +75,13 @@ public:
         for(int i=0; i<n;i++){
             res[i]=s;
         }
+        vector<int> leftrow(n,0), upperdiag(2*n-1,0),lowerdiag(2*n-1,0);
+        solve(0,n,res,result,leftrow,upperdiag,lowerdiag);
+        /*
         posi(res,result,n,0);
+        */
         return result;
+
+
     }
 };
