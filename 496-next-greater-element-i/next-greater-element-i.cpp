4c2315813f7109e051951dee5c0f4940a35ed017
EvilDeadLoc <rishabnawani8@gmail.com>
2025-11-05 23:09:18 +0530

    Time: 7 ms (12.26%) | Memory: 12.2 MB (90.95%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+
         vector<int> result;
         for(int i=0;i<nums1.size();i++){
             bool found=false;
