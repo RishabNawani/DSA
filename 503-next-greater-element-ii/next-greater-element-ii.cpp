7e8f3de30883492f4a6bc6c4fd2fcced73c58db9
EvilDeadLoc <rishabnawani8@gmail.com>
2025-11-06 22:49:08 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (89.79%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n, -1);
+        stack<int> st; // store indices
+        
+        for (int i = 0; i < 2 * n; i++) {
+            int num = nums[i % n];
+            while (!st.empty() && nums[st.top()] < num) {
+                res[st.top()] = num;
+                st.pop();
+            }
+            if (i < n) st.push(i);
+        }
+        return res;
+    /*
         vector<int> ress;
         for(int i=0; i<nums.size();i++){
             bool found=false;
@@ -16,5 +30,6 @@ public:
             ress.push_back(add);
         }
         return ress;
+        */
     }
 };
