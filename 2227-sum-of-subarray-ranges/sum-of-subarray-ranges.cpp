59cc8b53f3ede7d95af3db324195befae65b96d9
EvilDeadLoc <rishabnawani8@gmail.com>
2025-11-12 21:54:06 +0530

    Time: 23 ms (8.12%) | Memory: 13.9 MB (89.75%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -1,52 +1,18 @@
 class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
-         // Size of array
-        int n = nums.size();
-        
-        // To store the sum
-        long long sum = 0;
-        
-        // Traverse on the array
-        for(int i=0; i < n; i++) {
-            
-            // To store the smallest value of subarray
-            int smallest = nums[i];
-            
-            // To store the largest value of subarray
-            int largest = nums[i];
-            
-            /* Nested loop to get all 
-            subarrays starting from index i */
-            for(int j=i; j < n; j++) {
-                
-                // Update the smallest value
-                smallest = min(smallest, nums[j]);
-                
-                // Update the largest value
-                largest = max(largest, nums[j]);
-                
-                // Update the sum
-                sum += (largest - smallest);
-            }
-        }
-        
-        // Return the computed sum
-        return sum;
-        /*
-
-        int sum=0;
+        long long sum=0;
         for(int i=0;i<nums.size();i++){
             int maxx=nums[i];
             int minn=nums[i];
             for(int j=i;j<nums.size();j++){
                 maxx=max(nums[j],maxx);
-                minn=min(nums[j],maxx);
-                sum=sum+ maxx-minn;
+                minn=min(nums[j],minn);
+                sum+=maxx-minn;
             }
         }
-        return (long long)sum;
-    
+        return sum;
+    /*
         vector<vector<int>> sumarray;
         long long sum=0;
         for(int i=0;i<nums.size();i++){
