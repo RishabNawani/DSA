eaed8ac38c75da8670c0779e0f6abcfd0be0a28e
EvilDeadLoc <rishabnawani8@gmail.com>
2025-11-29 14:34:41 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (57.80%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -1,5 +1,24 @@
 class Solution {
 public:
+
+    int largee(vector<int>& heights){
+        stack<int> st;
+        heights.push_back(0);
+        int maxx=0;
+
+        for(int i=0;i<heights.size();i++){
+            while(!st.empty() && heights[st.top()]>heights[i]){
+                int h=heights[st.top()];
+                st.pop();
+                int left=st.empty()?-1:st.top();
+                int width=i-left-1;
+                maxx=max(maxx,h*width);
+            }
+            st.push(i);
+        }
+        return maxx;
+    }
+/*
     int lhist(int r,vector<vector<int>>& pree){
         int large=0;
         
@@ -17,9 +36,23 @@ public:
         }
         return large;
     }
+*/
     int maximalRectangle(vector<vector<char>>& matrix) {
         int m=matrix.size();
         int n=matrix[0].size();
+
+        vector<int> heightss(n,0);
+        int maxxx=0;
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                heightss[j]+=1;
+                if(matrix[i][j]=='0') heightss[j]=0;
+            }
+            maxxx=max(maxxx,largee(heightss));
+        }
+        return maxxx;
+        /*
         vector<vector<int>> presum(m,vector<int>(n,0));
         for(int i=0; i<matrix.size();i++){
             int sum=0;
@@ -35,5 +68,6 @@ public:
         }
 
         return maxx;
+        */
     }
 };
