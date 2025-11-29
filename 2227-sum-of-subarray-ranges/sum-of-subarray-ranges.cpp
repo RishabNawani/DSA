2c784494baead79f2db3958a5e482408067581fb
EvilDeadLoc <rishabnawani8@gmail.com>
2025-11-29 15:49:16 +0530

    Time: 23 ms (8.75%) | Memory: 13.9 MB (89.23%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -1,6 +1,43 @@
 class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
+        /*
+        int n = nums.size();
+        long long ans = 0;
+        stack<int> st;
+
+        // ---------- 1️⃣ Count contributions as MIN ----------
+        // We use <= in the comparison to avoid double-counting duplicates.
+        for (int i = 0; i <= n; ++i) {
+            while (!st.empty() && (i == n || nums[st.top()] >= nums[i])) {
+                int j = st.top();
+                st.pop();
+                int k = st.empty() ? -1 : st.top();
+                long long left = j - k;
+                long long right = i - j;
+                ans -= (long long)nums[j] * left * right;
+            }
+            st.push(i);
+        }
+
+        // ---------- 2️⃣ Count contributions as MAX ----------
+        while (!st.empty()) st.pop();  // reset stack
+
+        for (int i = 0; i <= n; ++i) {
+            while (!st.empty() && (i == n || nums[st.top()] <= nums[i])) {
+                int j = st.top();
+                st.pop();
+                int k = st.empty() ? -1 : st.top();
+                long long left = j - k;
+                long long right = i - j;
+                ans += (long long)nums[j] * left * right;
+            }
+            st.push(i);
+        }
+
+        return ans;
+        */
+    
         long long sum=0;
         for(int i=0;i<nums.size();i++){
             int maxx=nums[i];
