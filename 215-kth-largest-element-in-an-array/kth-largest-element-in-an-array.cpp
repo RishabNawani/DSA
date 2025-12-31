42e4c6fcca379b06232d027c0cf73b923ddb4a55
EvilDeadLoc <rishabnawani8@gmail.com>
2025-12-31 14:26:18 +0530

    Time: 32 ms (55.27%) | Memory: 59.2 MB (85.72%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-/*
+
     void heapify(vector<int>& nums,int n, int i){
         int largest=i;
-        int left=2*i;
-        int right=2*i+1;
+        int left=2*i+1;
+        int right=2*i+2;
         if(left<n && nums[largest]<nums[left]) largest=left;
         if(right<n && nums[largest]<nums[right]) largest=right;
 
@@ -13,20 +13,22 @@ public:
             heapify(nums,n,largest);
         }
     }
-*/
+
     int findKthLargest(vector<int>& nums, int k) {
         
-        int n=nums.size()-1;
-/*
-        for(int i=n/2;i>0;i--){
+        int n=nums.size();
+
+        for(int i=n/2-1;i>=0;i--){
             heapify(nums,n,i);
         }
 
-        return nums[0];
-        */
 
-        sort(nums.begin(),nums.end());
-        return nums[n-k+1];
+        for(int i=0;i<k-1;i++){
+            swap(nums[0],nums[n-i-1]);
+            heapify(nums,n-1-i,0);
+        }
+
+        return nums[0];
 
     }   
 };
