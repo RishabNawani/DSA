39d8f57a56849cc591827f9d1a7eaf91a0828f00
EvilDeadLoc <rishabnawani8@gmail.com>
2026-01-06 19:14:34 +0530

    Time: 6 ms (21.06%) | Memory: 20.1 MB (11.11%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
+
+        
         if(nums.size()==1) return nums;
         if(nums.empty()) return {};
+        /*
         map<int,int> storage;
 
         for(int num:nums){
@@ -26,5 +29,36 @@ public:
         }
 
         return storee;
+        */
+
+        unordered_map<int ,int> mpp;
+        int n=nums.size();
+
+        for(int num:nums){
+            mpp[num]++;
+        }
+    
+
+        vector<vector<int>>bucket(n+1);
+
+        for(auto it : mpp){
+            bucket[it.second].push_back(it.first);
+        }
+
+        vector<int> ress;
+
+        for(int i=n;i>=0;i--){
+            if(bucket[i].size()==0) continue;
+
+            while(bucket[i].size()>0 && k>0){
+                ress.push_back(bucket[i].back());
+                bucket[i].pop_back();
+                k--;
+            }
+
+        }
+
+        return ress;
+
     }
 };
