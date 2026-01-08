55040a6723308d5751a10567019b834dba02233e
EvilDeadLoc <rishabnawani8@gmail.com>
2026-01-08 19:51:07 +0530

    Time: 16 ms (11.23%) | Memory: 44.8 MB (44.33%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int cnt=0;
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int n=g.size();
+        int m=s.size();
+        int i=0;
+        int j=0;
+        while(i<n && j<m ){
+            if(g[i]<=s[j]){
+                cnt++;
+                i++;
+            }
+            j++;
+        }
+
+        return cnt;
+    }
+};
