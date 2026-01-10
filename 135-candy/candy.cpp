bf387bb736ef24c77882f939352ad9c9f2505808
EvilDeadLoc <rishabnawani8@gmail.com>
2026-01-10 13:35:31 +0530

    Time: 0 ms (100.00%) | Memory: 22.1 MB (84.57%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -10,6 +10,8 @@ public:
     int candy(vector<int>& ratings) {
 
         int n=ratings.size();
+
+        /*
         vector<int> leftt(n,1);
         for(int i=1;i<n;i++){
             if(ratings[i]>ratings[i-1]){
@@ -40,6 +42,43 @@ public:
 
 
         return summ;
+        */
+
+/// approach 3 - uses slope method , it checks on two slopes , first the increasing then the decreasing , in the
+/// increasing one , it looks till the peak value , till when it keeps increasing and for the decreasing value it
+/// looks at the values after the peak element till it starts to remain stagnant or begans to increase again.
+
+        int sum=1;
+        int i=1;
+
+        while(i<n){
+            if(ratings[i]==ratings[i-1]){
+                sum+=1;
+                i++;
+                continue;
+            }
+
+            int peak=1;
+            while(i<n && ratings[i]>ratings[i-1]){
+                peak++;
+                sum+=peak;
+                i++;
+            }
+
+            int down=1;
+
+            while(i<n && ratings[i]<ratings[i-1]){
+                sum+=down;
+                down++;
+                i++;
+            }
+
+            if(down>peak){
+                sum+= (down-peak);
+            }
+        }
+
+        return sum;
 
     }
 };
