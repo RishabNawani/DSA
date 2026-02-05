74301f45ae6383cda1849d9c0fc91b9ff36921c8
EvilDeadLoc <rishabnawani8@gmail.com>
2026-02-05 23:29:50 +0530

    Time: 3 ms (54.74%) | Memory: 37.3 MB (63.08%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -9,9 +9,42 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+class BSTIterator {
+public:
+    stack<TreeNode*> ss;
+    bool rev;
+    void pushh(TreeNode* root){
+        while(root!=NULL){
+            ss.push(root);
+            if(rev==true)root=root->right;
+
+            else root=root->left;
+        }
+    }
+
+    BSTIterator(TreeNode* root, bool isrev) {
+        rev=isrev;
+        pushh(root);
+    }
+    
+    int next() {
+        TreeNode* topp= ss.top();
+        ss.pop();
+        if(!rev) pushh(topp->right);
+        pushh(topp->left);
+        return topp->val;
+    }
+    
+    bool hasNext() {
+        return !ss.empty();
+    }
+};
+
+
 class Solution {
 public:
 
+/*
     void inn(TreeNode* root,vector<int>& in){
         if(root==NULL) return ;
         
@@ -20,7 +53,9 @@ public:
         inn(root->right,in);
         
     }
+    */
     bool findTarget(TreeNode* root, int k) {
+        /*
         vector<int> ss;
         inn(root,ss);
 
@@ -34,6 +69,21 @@ public:
             if(ss[l]+ss[r]<k) l++;
             if(ss[l]+ss[r]>k) r--;
         }
+        return false;
+        */
+        if(!root) return false;
+        BSTIterator l(root,false);
+        BSTIterator r(root,true);
+
+        int i=l.next();
+        int j=r.next();
+
+        while(i<j){
+            if(i+j==k) return true;
+            if(i+j<k) i=l.next();
+            if(i+j>k) j=r.next();
+        }
+
         return false;
     }
 };
