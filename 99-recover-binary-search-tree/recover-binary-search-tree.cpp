79cb64d7f7397ea56cacc2180aeb240a95a6abd1
EvilDeadLoc <rishabnawani8@gmail.com>
2026-02-06 21:04:04 +0530

    Time: 0 ms (100.00%) | Memory: 60.9 MB (27.48%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/recover-binary-search-tree.cpp
@@ -11,6 +11,31 @@
  */
 class Solution {
 public:
+    TreeNode* prev;
+    TreeNode* first;
+    TreeNode* middle;
+    TreeNode* last;
+
+    void joinn(TreeNode* root){
+        if(root==NULL) return;
+
+        joinn(root->left);
+
+        if(prev!=NULL && (root->val < prev->val)){
+
+            if(first==NULL){
+                first=prev;
+                middle=root;
+            }
+            else{
+                last=root;
+            }
+        }
+        prev=root;
+        joinn(root->right);
+    }
+
+/*
     void innn(TreeNode* root,vector<int>& in){
         if(root==NULL) return ;
         
@@ -25,14 +50,23 @@ public:
         root->val=ii[i++];
         create(root->right,i,ii);
     }
+    */
 
     void recoverTree(TreeNode* root) {
+
+        first=middle=last=NULL;
+        prev = new TreeNode(INT_MIN);
+        joinn(root);
+        if(first && last)  swap(first->val,last->val);
+        else if(first && middle)  swap(first->val, middle->val);
+
+        /*
         vector<int> inn;
         innn(root,inn);
 
         sort(inn.begin(),inn.end());
         int i=0;
         create(root,i,inn);
-        
+        */
     }
 };
