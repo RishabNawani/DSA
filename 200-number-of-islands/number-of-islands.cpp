e76fdd4e03ecf7f171ae2d01d859b90390a02c05
EvilDeadLoc <rishabnawani8@gmail.com>
2026-02-21 16:27:33 +0530

    Time: 28 ms (49.46%) | Memory: 22.1 MB (34.72%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,6 +1,36 @@
 class Solution {
 public:
+    void dfs(int r, int c, int br , int bc ,vector<vector<int>> &viss,vector<vector<char>>& grid,vector<pair<int,int>> &shape){
+        viss[r][c]=1;
+        shape.push_back({r-br,c-bc});
+        int drow[]={-1,0,1,0};
+        int dcol[]={0,1,0,-1};
+        for(int i=0;i<4;i++){
+            int nr=r+drow[i];
+            int nc=c+dcol[i];
+
+            if(nr>=0 && nr<grid.size() && nc>=0 && nc<grid[0].size() && !viss[nr][nc] && grid[nr][nc]=='1'){
+                dfs(nr,nc,br,bc,viss,grid,shape);
+            }
+        }
+    }
     int numIslands(vector<vector<char>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>> viss(n,vector<int>(m,0));
+        vector<vector<pair<int,int>>> stt;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1' && !viss[i][j]){
+                    vector<pair<int,int>> shape;
+                    dfs(i,j,i,j,viss,grid,shape);
+                    stt.push_back(shape);
+                }
+            }
+        }
+        return stt.size();
+        /*
         int n=grid.size();
         int m=grid[0].size();
         queue<pair<int,int>> qq; // ((row,col))
@@ -30,5 +60,6 @@ public:
             }
         }
         return cnt;
+        */
     }
 };
