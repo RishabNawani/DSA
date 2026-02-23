ec7b6f67c4a257f984819b43747a2ec83a12dcb6
EvilDeadLoc <rishabnawani8@gmail.com>
2026-02-23 17:00:29 +0530

    Time: 4 ms (56.58%) | Memory: 18.5 MB (53.23%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    bool dfs(vector<vector<int>>& adjj,vector<int>& viss, int i,vector<int>& ordd){
+        /*
+        if(viss[i]==1) return true;
+        if(viss[i]==2) return false;
+        viss[i]=1;
+        for(int neigh:adjj[i]){
+            if(dfs(adjj,viss,neigh)) return true;
+        }
+
+        viss[i]=2;
+        return false;
+        */
+        viss[i]=1;
+        for(auto adj:adjj[i]){
+            if(!viss[adj]){
+                if(dfs(adjj,viss,adj,ordd)) return true; /// here this approach fails because we use the parent appraoch which will work in undirected but not in directed and here the graph is directed graph.
+            }
+            else if(viss[adj]==1) return true;
+        }
+        viss[i]=2;
+        ordd.push_back(i);
+        return false;
+    }
+
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adjj(numCourses);
+
+        vector<int> ordd;
+        for(auto &p: prerequisites){
+            int b=p[0];
+            int a=p[1];
+
+            adjj[a].push_back(b);
+        }
+
+        vector<int> viss(numCourses,0);
+
+
+        for(int i=0;i<numCourses;i++){
+            if(!viss[i]){
+                if(dfs(adjj,viss,i,ordd)) return {};
+            }
+        }
+        reverse(ordd.begin(),ordd.end());
+        return ordd;
+    }
+};
