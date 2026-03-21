52ad8fc0025d75b707bd816aebb4a61938f0b23c
EvilDeadLoc <rishabnawani8@gmail.com>
2026-03-21 18:51:25 +0530

    Time: 12 ms (79.63%) | Memory: 21.3 MB (55.95%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -1,5 +1,83 @@
+class DSU {
+public:
+    unordered_map<int, int> parent;
+
+    // Function to find the ultimate parent of node x
+    int find(int x) {
+        // If x is not present, make it its own parent
+        if (parent.find(x) == parent.end())
+            parent[x] = x;
+
+        // Path compression to optimize future finds
+        if (x != parent[x])
+            parent[x] = find(parent[x]);
+
+        return parent[x];
+    }
+
+    // Function to connect two nodes
+    void unite(int x, int y) {
+        // Set parent of x's root to y's root
+        parent[find(x)] = find(y);
+    }
+};
+
 class Solution {
 public:
+    int removeStones(vector<vector<int>>& stones) {
+        DSU dsu;
+
+        // Connect each stone's row and column (offset to avoid collision)
+        for (auto& stone : stones) {
+            dsu.unite(stone[0], stone[1] + 10001);
+        }
+
+        unordered_set<int> components;
+
+        // Count unique connected components
+        for (auto& stone : stones) {
+            components.insert(dsu.find(stone[0]));
+        }
+
+        // Max stones removed = total stones - number of components
+        return stones.size() - components.size();
+    }
+};
+/*
+class Diss{
+        vector<int> rank , parent;
+public:
+    Diss(int n){
+        rank.resize(n+1);
+        parent.resize(n+1);
+        for(int i=0;i<=n;i++){
+            parent[i]=i;
+        }
+    }
+
+    int findupar(int node){
+        if(node==parent[node]){
+            return node;
+        }
+        return parent[node]= findupar(parent[node]);
+    }
+
+    void unionbyrank(int u, int v){
+        int ulpu=findupar(v);
+        int ulpv=findupar(v);
+        if(ulpu=ulpv) return;
+        if(rank[ulpu]<rank[ulpv]) parent[ulpu]=ulpv;
+        else if(rank[ulpu]>rank[ulpv]) parent[ulpv]=ulpu;
+        else{
+            parent[ulpv]=ulpu;
+            rank[ulpu]++;
+        }
+    }
+};
+class Solution {
+public:
+
+   
     void dfs(vector<vector<int>>& adjj,vector<int>& viss, int i){
         viss[i]=1;
         for(int it:adjj[i]){
@@ -9,7 +87,17 @@ public:
         }
     }
     int removeStones(vector<vector<int>>& stones) {
+        int maxcol=0;
+        int maxrow=0;
+
         if(stones.size()==1) return 0;
+        for(auto it:stones){
+            maxrow=max(maxrow,it[0]);
+            maxcol=max(maxcol,it[1]);
+        }
+
+        diss(maxrow+maxcol+1);
+
         int n=stones.size();
         vector<vector<int>> adjj(n);
         for(int i=0;i<n;i++){
@@ -28,9 +116,7 @@ public:
                 dfs(adjj,viss,i);
             }
         }
-    
-
         return n-cnt;
-
     }
 };
+*/
