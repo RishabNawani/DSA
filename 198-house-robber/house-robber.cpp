b7a5581929c690e1de4c3016547814137c6a5101
EvilDeadLoc <rishabnawani8@gmail.com>
2026-03-31 14:13:29 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (5.45%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
+    int robb(vector<int>& nums,int i,vector<int>& dp){
+        if(i<0) return 0;
+        if(i==0) return nums[0];
+        if(dp[i]!=-1) return dp[i];
+
+        int pickk=nums[i]+robb(nums,i-2,dp);
+        int notpickk=robb(nums,i-1,dp);
+
+        return dp[i]=max(pickk,notpickk);
+    }
+
     int rob(vector<int>& nums) {
+        /*
+        TABULATION METHOD
+
         int n=nums.size();
 
         if(n==1) return nums[0];
@@ -12,5 +26,12 @@ public:
         }      
 
         return dp[n-1];
+        */
+
+        /* Memoization method*/
+
+        int n=nums.size();
+        vector<int> dp(n,-1);
+        return robb(nums,n-1,dp);
     }
 };
