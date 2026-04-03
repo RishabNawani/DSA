4adefab25daf9c8ccc73992269b22532d71108d8
EvilDeadLoc <rishabnawani8@gmail.com>
2026-04-03 14:29:26 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (97.05%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+/* memoization
     int f(int i,int j,vector<vector<int>> &dp){
         if(i==0 && j==0) return 1;
         if(i<0 || j<0) return 0;
@@ -8,7 +9,7 @@ public:
         int left=f(i,j-1,dp);
         return dp[i][j]=up+left;
     }
-/*
+
 Recursion
     int f(int i,int j){
         if(i==0 && j==0) return 1;
@@ -18,9 +19,28 @@ Recursion
         return up+left;
     }
 */
+
+
     int uniquePaths(int m, int n) {
+        /* memoization
         vector<vector<int>> dp(m,vector(n,-1));
        /// recursive->  return f(m-1,n-1);
        return f(m-1,n-1,dp);
+       */
+       int dp[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) dp[i][j]=1;
+                else{
+                    int up=0;
+                    int left=0;
+                    if(i>0) up=dp[i-1][j];
+                    if(j>0) left=dp[i][j-1];
+                    dp[i][j]=up+left;
+                }
+            }
+        }
+
+        return dp[m-1][n-1];
     }
 };
