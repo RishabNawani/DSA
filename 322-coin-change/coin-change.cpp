c1f194321a21ac3701ee84e2c24910048376273f
EvilDeadLoc <rishabnawani8@gmail.com>
2026-04-08 22:46:56 +0530

    Time: 42 ms (40.78%) | Memory: 48.9 MB (34.69%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -28,6 +28,8 @@ public:
         if(a>=1e9) return -1;
         return a;
         */
+
+        /* memoization solution
     int ans(int i,vector<int> &coins, int tar,vector<vector<int>> &dp){
         if(i==0){
             if(tar%coins[i]==0) return tar/coins[i];
@@ -46,5 +48,27 @@ public:
         int a=ans(n-1,coins,amount,dp);
         if(a>=1e9) return -1;
         return a;
+        */
+        int coinChange(vector<int>& coins, int amount) {
+            int n=coins.size();
+            vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+            for(int t=0;t<=amount;t++){
+                if(t % coins[0]==0)dp[0][t]=t/coins[0];
+                else dp[0][t]=1e9;
+            }
+
+            for(int i=1;i<n;i++){
+                for(int t=0;t<=amount;t++){
+                    int nottake=0+dp[i-1][t];
+                    int take=INT_MAX;
+                    if(coins[i]<=t) take=1+dp[i][t-coins[i]];
+                    dp[i][t]=min(take,nottake);
+                }
+
+            }
+            
+            int a=dp[n-1][amount];
+            if(a>=1e9) return -1;
+            return a;
     }
 };
