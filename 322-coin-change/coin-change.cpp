ce4ceea1b26eb2d9fd592e5c34e18f440d9891b5
EvilDeadLoc <rishabnawani8@gmail.com>
2026-04-08 22:39:27 +0530

    Time: 40 ms (42.13%) | Memory: 49.3 MB (30.65%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,50 @@
+/*
+can be solved using take and non take , whenever you choose non take , set not take = 0+f(ind-1,target), move the index left , whenevr you choose take , choose it only when coins[ind]<=target and take=1+f(ind, target-coin[ind]), we stay at the same place because there are an infinite number of coins present.
+
+so , we store the targets previously , because due to recursion , tc and sc were coming hugeeeeeee, which gave error
+
+tabulation is the wayyy.
+*/
+
+
+class Solution {
+public:
+/* recursion solution
+    int ans(int i,vector<int> &coins, int tar){
+        if(i==0){
+            if(tar%coins[i]==0) return tar/coins[i];
+            else return 1e9;
+        }
+
+        int nottake=0+ans(i-1,coins,tar);
+        int take=INT_MAX;
+        if(coins[i]<=tar) take=1+ans(i,coins,tar-coins[i]);
+
+        return min(take,nottake);
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        int n=coins.size();
+        int a=ans(n-1,coins,amount);
+        if(a>=1e9) return -1;
+        return a;
+        */
+    int ans(int i,vector<int> &coins, int tar,vector<vector<int>> &dp){
+        if(i==0){
+            if(tar%coins[i]==0) return tar/coins[i];
+            else return 1e9;
+        }
+        if( dp[i][tar]!=-1) return dp[i][tar];
+        int nottake=0+ans(i-1,coins,tar,dp);
+        int take=INT_MAX;
+        if(coins[i]<=tar) take=1+ans(i,coins,tar-coins[i],dp);
+
+        return dp[i][tar]=min(take,nottake);
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        int n=coins.size();
+        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        int a=ans(n-1,coins,amount,dp);
+        if(a>=1e9) return -1;
+        return a;
+    }
+};
