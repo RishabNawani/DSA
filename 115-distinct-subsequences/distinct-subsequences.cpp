f3ae43313c38c27e6983f878c286f2dac586d4c8
EvilDeadLoc <rishabnawani8@gmail.com>
2026-04-12 15:26:54 +0530

    Time: 27 ms (69.55%) | Memory: 44.3 MB (23.15%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+/*memoization
     int f(string &s, string &t,int i , int j,vector<vector<int>> &dp){
         if(j<0) return 1;
         if(i<0) return 0;
@@ -12,11 +13,28 @@ public:
         return dp[i][j]=f(s,t,i-1,j,dp);
         
     }
+*/
 
+/* tabulation solution gives runtime error when using int and long long , so use double*/
     int numDistinct(string s, string t) {
         int n=s.size();
         int m=t.size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return f(s,t,n-1,m-1,dp);
+        vector<vector<double>> dp(n+1,vector<double>(m+1,0));
+        for(int i=0;i<=n;i++) dp[i][0]=1;
+       /// can be removed as already all of it is set to 0 for(int j=1;j<=m;j++) dp[0][j]=0;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+
+        return (int)dp[n][m];
+        //return f(s,t,n-1,m-1,dp);
     }
 };
