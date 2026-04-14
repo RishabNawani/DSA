ee504af54e49f0ac3dcb203db588eb1215a100e8
EvilDeadLoc <rishabnawani8@gmail.com>
2026-04-14 14:00:23 +0530

    Time: 62 ms (30.91%) | Memory: 14 MB (70.84%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
+/*
     bool f(int i, int j, string &s, string &p, vector<vector<int>> &dp){
+    
         if(i<0 && j<0) return true;
         if(j>=0 && i<0) return false;
         if(j<0 && i>=0){
@@ -19,13 +21,40 @@ public:
         return false;
         
     }
-
+*/
     bool isMatch(string s, string p) {
         int n=p.size();
         int m=s.size();
+        vector<vector<bool>> dp(n+1,vector<bool> (m+1,0));
 
-        vector<vector<int>> dp(n,vector<int>(m,-1));
+        dp[0][0]=true;
+        for(int j=1;j<=m;j++) dp[0][j]=false;
+        for(int i=1;i<=n;i++){
+            bool fl=true;
+            for(int k=1;k<=i;k++){
+                if(p[k-1]!='*'){
+                    fl=false;
+                    break;
+                }
+            }
+            dp[i][0]=fl;    
+        }
+
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[j-1]==p[i-1] || p[i-1]=='?') dp[i][j]=dp[i-1][j-1];
+
+                else if(p[i-1]=='*') dp[i][j]=dp[i-1][j] | dp[i][j-1];
+
+                else dp[i][j]=false;
+            }
+        }
+
+        return dp[n][m];
+        /*vector<vector<int>> dp(n,vector<int>(m,-1));
 
         return f(n-1,m-1,s,p,dp);
+        */
     }
 };
