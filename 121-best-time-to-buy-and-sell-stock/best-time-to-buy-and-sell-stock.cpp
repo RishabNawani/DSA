73d74cf8b06758480be67c7f4d783f7c0bcf6f76
EvilDeadLoc <rishabnawani8@gmail.com>
2026-04-15 12:01:45 +0530

    Time: 0 ms (100.00%) | Memory: 97.4 MB (24.16%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        
         int minPrice = INT_MAX;
         int maxProfit = 0;
         
@@ -12,5 +13,16 @@ public:
         }
         
         return maxProfit;    
+        /*
+        int maxx=0;
+        for(int i=0;i<prices.size();i++){
+            for(int j=i+1;j<prices.size();j++){
+                int proff=prices[j]-prices[i];
+                maxx=max(maxx,proff);
+            }
+        }
+
+        return maxx;
+        */
     }
 };
