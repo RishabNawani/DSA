7d99667accf3cfb2227cb1c9b5adb1d9df8b7514
EvilDeadLoc <rishabnawani8@gmail.com>
2026-04-29 13:18:28 +0530

    Time: 8 ms (35.09%) | Memory: 12.4 MB (59.13%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -20,8 +20,26 @@ public:
     }
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n=arr.size();
-        vector<int> dp(n,-1);
+        //vector<int> dp(n,-1);
 
-        return f(0,arr,k,dp);
+        //return f(0,arr,k,dp);
+        vector<int> dp(n+1,0);
+        for(int ind=n-1;ind>=0;ind--){
+            int len=0;
+            int maxx=INT_MIN;
+            int maxans=INT_MIN;
+
+            for(int j=ind;j<min(ind+k,n);j++){
+                len++;
+                maxx=max(maxx,arr[j]);
+                int sum=len*maxx+dp[j+1];
+                maxans=max(maxans,sum);
+
+            }
+
+            dp[ind]=maxans;
+        }
+
+        return dp[0];
     }
 };
