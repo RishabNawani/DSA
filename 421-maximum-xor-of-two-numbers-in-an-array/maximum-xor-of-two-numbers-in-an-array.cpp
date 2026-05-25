5e6bc3948bcd4b95a6cbaa398f6a32b6a17fbb3c
EvilDeadLoc <rishabnawani8@gmail.com>
2026-05-25 17:43:34 +0530

    Time: 573 ms (84.79%) | Memory: 184.1 MB (77.03%) - LeetSync

---
+++ 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.cpp
@@ -0,0 +1,66 @@
+class Node{
+public:
+    Node* links[2];
+
+    bool containskey(int bit){
+        return links[bit]!=NULL;
+    }
+
+    Node* get(int bit){
+        return links[bit];
+    }
+
+    void put(int bit,Node* node){
+        links[bit]=node;
+    }
+};
+
+class Solution {
+public:
+    Node* root;
+
+    Solution(){
+        root= new Node();
+    }
+
+    void insert(int num) {
+        Node* node = root;
+        for (int i = 31; i >= 0; i--) {
+        
+            int bit = (num >> i) & 1;
+
+            if (!node->containskey(bit)) {
+                node->put(bit, new Node());
+            }
+
+            node = node->get(bit);
+        }
+    }
+
+    int getMaxXOR(int num) {
+        Node* node = root;
+        int maxXor = 0;
+
+        for (int i = 31; i >= 0; i--){
+            int bit = (num >> i) & 1;
+            if (node->containskey(1 - bit)) {
+                maxXor |= (1 << i);
+                node = node->get(1 - bit);
+            } else {
+                node = node->get(bit);
+            }
+        }
+        return maxXor;
+    }
+
+    int findMaximumXOR(vector<int>& nums) {
+        for(int num : nums){
+            insert(num);
+        }
+        int maxResult = 0;
+        for(int num : nums){
+            maxResult = max(maxResult, getMaxXOR(num));
+        }
+        return maxResult;
+    }
+};
